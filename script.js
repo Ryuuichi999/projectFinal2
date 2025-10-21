ffccfed40b5a77e1d6add3f424042c883a252b0d
Ryuuichi999 <Ryuuichii09@gmail.com>
2025-10-21 18:03:33 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// ปุ่มเมนูมือถือ
+document.getElementById('mobileMenuButton').addEventListener('click', () => {
+  alert('ไม่มีเมนูด้านข้างในโหมดนี้');
+});
+
+// ปุ่ม About
+document.getElementById('aboutButton').addEventListener('click', () => {
+  window.location.href = 'about.html';
+});
