4b974291ce7551cf8e86311a118448e528064a3d
Ryuuichi999 <Ryuuichii09@gmail.com>
2025-10-22 23:48:43 +0700

    add and edit

--- script.js
+++
@@ -1,9 +0,0 @@
-// ปุ่มเมนูมือถือ
-document.getElementById('mobileMenuButton').addEventListener('click', () => {
-  alert('ไม่มีเมนูด้านข้างในโหมดนี้');
-});
-
-// ปุ่ม About
-document.getElementById('aboutButton').addEventListener('click', () => {
-  window.location.href = 'about.html';
-});
